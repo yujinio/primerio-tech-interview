4cf7b02f4faa847499c40ed52e333c9c8d892506
yujinio <root@yujin.io>
2024-11-18 20:10:48 +0100

    reorganize

--- implementation/src/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from src.router.payments import router as payments_router
-from src.router.tokenization import router as tokenization_router
-
-app = FastAPI()
-
-app.include_router(tokenization_router)
-app.include_router(payments_router)
